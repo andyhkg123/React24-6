8f4d0b748180fc6b6a9c1161b3626c9f176e21b9
Andy Wong <andy.wongtszkiu@gmail.com>
2024-06-24 23:37:09 +0800

    back up

--- src/App.js
+++ src/App.js
@@ -4,15 +4,21 @@ import Navbar from "./components/Navbar";
 async function App() {
   //logic
   let result = "";
-  const response = await fetch("http://api.github.com/users/andyhkg123");
+  const response = await fetch(
+    "https://data.etabus.gov.hk/v1/transport/kmb/route/"
+  );
   result = await response.json();
-  console.log(result);
+  console.log(result.data);
   if (!result) {
     return <div>Loading.....</div>;
   }
   return (
     <div className="App">
       <Navbar />
+      {result &&
+        result.data.map((item) => {
+          return <div>{item.dest_tc}</div>;
+        })}
     </div>
   );
 }
