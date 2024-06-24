acac717ddca12d34ed2f31d1e74b04686e8b547e
Andy Wong <andy.wongtszkiu@gmail.com>
2024-06-24 14:06:05 +0800

    first React component

---
+++ src/components/Navbar.js
@@ -0,0 +1,24 @@
+import "../css/Navbar.css";
+import logo from "../logo.svg";
+
+function Navbar() {
+  return (
+    <nav>
+      <div className="nav-container">
+        <div>
+          <img src={logo} alt="logo" width="50px" height="50px" />
+        </div>
+        <div>
+          <ul className="menu">
+            <li>Name</li>
+            <li>Profile</li>
+            <li>Settings</li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+// Very important!!!
+export default Navbar;
